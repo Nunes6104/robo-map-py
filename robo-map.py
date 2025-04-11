30a2e81ed1a4c70059d922337d5ea1043aadd928
Nunes6104 <jmmnunes6104@gmail.com>
2025-04-11 21:40:53 +0100

    update

--- robo-map.py
+++ robo-map.py
@@ -1,6 +1,3 @@
-import os
-import shutil
-file = "test.txt"
 def ler_mapa(nome_ficheiro):
     mapa = []
     with open(nome_ficheiro, "r") as f:
@@ -11,48 +8,62 @@ def ler_mapa(nome_ficheiro):
             #para garantir que existe 10 elementos
             if len(elementos) == 10:
                 mapa.append(elementos)
-    mapa[0][0][0] == 'R'
+    mapa[0][0] = 'R'
     return mapa
+posX = 0
+posY = 0
+totalM = 0
 
 def moveUp(mapa):
-    if True:
-        for el in mapa:
-            print(' '.join(el))
-            
+    #se existir o ponto trocar por R
+    if mapa[posX - 1][posY] == '.':
+        mapa[posX - 1][posY] = 'R'
+    #caso contrário damos erro e voltamos para trás
+    elif mapa[posX - 1][posY] == '#':
+        print("Existe um obstáculo!")
+    elif mapa[posX - 1][posY] == 'R':
+        print("Estás a voltar para trás!")
+    else:
+        print("Não podes sair do mapa!")
 
-def moveDown():
-    print("hello")
-    
-def moveLeft():
-    print("hello")
-    
-def moveRight():
-    print("hello")
+def moveDown(mapa):
+    #se existir o ponto trocar por R
+    if mapa[posX + 1][posY] == '.':
+        mapa[posX + 1][posY] = 'R'
+    #caso contrário damos erro e voltamos para trás
+    elif mapa[posX + 1][posY] == '#':
+        print("Existe um obstáculo!")
+    elif mapa[posX + 1][posY] == 'R':
+        print("Estás a voltar para trás!")
+    else:
+        print("Não podes sair do mapa!")
 
 def menu():
-    if os.path.exists(file):
-        mapa = ler_mapa(file)
+        mapa = ler_mapa("mapa.txt")
+            
         while True:
-            print("Escolha uma opção W,A,S,D para mover o Robô \nOpção 0 para encerrar o programa")
+            for linha in mapa:
+                print(' '.join(linha))
+            
+            print("\nEscolha uma opção W,A,S,D para mover o Robô \nOpção 0 para encerrar o programa")
             opcao = input("Escolha uma opção: ")
 
             if opcao.lower() == "w":
                 moveUp(mapa)
             elif opcao.lower() == "a":
-                moveLeft()
-            elif opcao.lower() == "s":
-                moveDown()                
+                #moveLeft(mapa)
+                print("Hello")
+            elif opcao.lower() == "s":   
+                moveDown(mapa) 
             elif opcao.lower() == "d":
-                moveRight()                
+                #moveRight(mapa) 
+                print("Hello")
+                      
             elif opcao == "0":
                 print("Programa encerrado.")
-                os.remove(file)
                 break
             else:
                 print("Opção inválida. Tente novamente.")
                 menu()
-    else:
-        shutil.copy("mapa.txt", file)
-        menu()
 #executa o menu
 menu()
